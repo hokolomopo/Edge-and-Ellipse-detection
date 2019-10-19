1142d91d3b3228ebbc7bc8664b48c4e44092c4ec
Arnaud Delaunoy <arnaud.delaunoy@student.uliege.be>
2019-10-19 19:04:49 +0200

    sobel naive_gradient scharr stacking

--- edge.py
+++ edge.py
@@ -1,16 +1,81 @@
 import cv2
+import numpy as np
 
-def sobel_edge(img):
+def saturate_cast_uint8(img):
+	"""
+	Taken from practical sessions
+	"""
+	return np.where( img > 255.0, 255.0, np.where( img < 0.0, 0.0, img)).astype(np.uint8)
+
+def clean_grad(grad, saturate, threshold):
+	if saturate:
+		grad = saturate_cast_uint8(grad)
+	
+	if threshold is not None:
+		grad = np.where(grad > threshold, 255., 0.).astype(np.uint8)
+
+	return grad
+
+def high_pass_filter(img, kernel_size, strength):
+	lowPass = cv2.GaussianBlur(img, (kernel_size, kernel_size), 0)
+	return saturate_cast_uint8(strength * img - ( strength - 1.0) * lowPass)
+
+def sobel_edge(img, saturate = True, threshold = 32, filtering = True, filtering_kernel_size = 1, filtering_strength = 2, kernel_size = 1):
 	"""
 	https://docs.opencv.org/2.4/doc/tutorials/imgproc/imgtrans/sobel_derivatives/sobel_derivatives.html
 	TODO: Voir convention calcul des gradients (si ca se met sur la case haut, bas, gauche, droite ...) et indiquer dans le rapport
 	"""
-	gradientX = cv2.Sobel(img, -1, 1, 0, 1)
-	gradientY = cv2.Sobel(img, -1, 0, 1, 1)
+
+	if filtering:
+		img = high_pass_filter(img, filtering_kernel_size, filtering_strength)
+
+	gradientX = cv2.Sobel(img, -1, 1, 0, kernel_size)
+	gradientY = cv2.Sobel(img, -1, 0, 1, kernel_size)
 
 	absGradientX = cv2.convertScaleAbs(gradientX)
 	absGradientY = cv2.convertScaleAbs(gradientY)
 
 	grad = cv2.addWeighted(absGradientX, 0.5, absGradientY, 0.5, 0)
-	
-	return grad
+
+	return clean_grad(grad, saturate, threshold)
+
+def naive_gradient(img, saturate = True, threshold = 16, filtering = True, filtering_kernel_size = 1, filtering_strength = 2):
+	if filtering:
+		img = high_pass_filter(img, filtering_kernel_size, filtering_strength)
+
+	xFilter = np.array([[-1, 0, 1]])
+	yFilter = np.array([[-1], [0], [1]])
+
+	gradientX = cv2.filter2D(img, cv2.CV_64F, xFilter)
+	gradientY = cv2.filter2D(img, cv2.CV_64F, yFilter)
+
+	absGradientX = cv2.convertScaleAbs(gradientX)
+	absGradientY = cv2.convertScaleAbs(gradientY)
+
+	grad = cv2.addWeighted(absGradientX, 0.5, absGradientY, 0.5, 0)
+
+	return clean_grad(grad, saturate, threshold)
+
+def scharr_edge(img, saturate = True, threshold = 64, filtering = True, filtering_kernel_size = 5, filtering_strength = 2):
+	if filtering:
+		img = high_pass_filter(img, filtering_kernel_size, filtering_strength)
+
+	gradientX = cv2.Scharr(img, cv2.CV_32F, 1, 0)/4.0
+	gradientY = cv2.Scharr(img, cv2.CV_32F, 0, 1)/4.0
+
+	absGradientX = cv2.convertScaleAbs(gradientX)
+	absGradientY = cv2.convertScaleAbs(gradientY)
+
+	grad = cv2.addWeighted(absGradientX, 0.5, absGradientY, 0.5, 0)
+
+	return clean_grad(grad, saturate, threshold)
+
+def stacking(img, saturate = True):
+	naiveGrad = naive_gradient(img)
+	sobel = sobel_edge(img)
+	scharr = scharr_edge(img)
+
+	grad = cv2.addWeighted(naiveGrad, 1/3, sobel, 1/3, 0)
+	grad = cv2.addWeighted(grad, 1., scharr, 1/3, 0)
+
+	return clean_grad(grad, saturate, 128)
--- main.py
+++ main.py
@@ -5,25 +5,108 @@ import argparse
 from image import *
 from edge import *
 
-def show_main():
+def show_main(imageName):
     # Load the image
-    img = load_gray_img('img/building.png')
+    img = load_gray_img(iamgeName)
     display_img(img)
 
-def sobel_main():
-    img = load_gray_img('img/building.png')
-    grad = sobel_edge(img)
-    display_img(grad)
+def method_main(imageName, method):
+    img = load_gray_img(imageName)
+    grad_no_saturate = method(img, False)
+    grad_saturate = method(img, True)
+    grad = np.concatenate((img, grad_no_saturate, grad_saturate), axis = 1)
+    display_img(grad, True)
+
+def threshold_main(imageName, method):
+    img = load_gray_img(imageName)
+    grad_4 = method(img, threshold = 4)
+    grad_6 = method(img, threshold = 6)
+    grad_8 = method(img, threshold = 8)
+    grad_16 = method(img, threshold = 16)
+    grad_32 = method(img, threshold = 32)
+    grad_64 = method(img, threshold = 64)
+    grad_96 = method(img, threshold = 96)
+    grad_128 = method(img, threshold = 128)
+    grad = np.concatenate((img, grad_4, grad_6, grad_8, grad_16, grad_32, grad_64, grad_96, grad_128), axis = 1)
+    display_img(grad, True)
+
+def filtering_main(imageName, method):
+    img = load_gray_img(imageName)
+    no_filter = method(img, filtering = False)
+    filter_ = method(img, filtering = True)
+    grad = np.concatenate((img, no_filter, filter_), axis = 1)
+    display_img(grad, True)
+
+def filter_kernel_main(imageName, method):
+    img = load_gray_img(imageName)
+    kernel_1 = method(img, filtering = True, filtering_kernel_size = 1)
+    kernel_3 = method(img, filtering = True, filtering_kernel_size = 3)
+    kernel_5 = method(img, filtering = True, filtering_kernel_size = 5)
+    kernel_7 = method(img, filtering = True, filtering_kernel_size = 7)
+    grad = np.concatenate((img, kernel_1, kernel_3, kernel_5, kernel_7), axis = 1)
+    display_img(grad, True)
+
+def filter_strength_main(imageName, method):
+    img = load_gray_img(imageName)
+    strength_1 = method(img, filtering = True, filtering_strength = 1)
+    strength_2 = method(img, filtering = True, filtering_strength = 2)
+    strength_3 = method(img, filtering = True, filtering_strength = 3)
+    strength_5 = method(img, filtering = True, filtering_strength = 5)
+    grad = np.concatenate((img, strength_1, strength_2, strength_3, strength_5), axis = 1)
+    display_img(grad, True)
+
+def compare_gradient_main(imageName):
+    img = load_gray_img(imageName)
+    grad_sobel = sobel_edge(img)
+    grad_naive = naive_gradient(img)
+    grad_scharr = scharr_edge(img)
+    grad_majority = majority_voter(img)
+    grad = np.concatenate((img, grad_naive, grad_sobel, grad_scharr, grad_majority), axis = 1)
+    display_img(grad, True)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='')
-    parser.add_argument("-mode", type=str, choices=["show", "sobel"], default='show')
+    parser.add_argument("-mode", type=str, choices=["show", "sobel", "naive_gradient", "scharr", "stacking", "compare_gradient"], default='show')
+    parser.add_argument("-task", type=str, choices=["main", "threshold", "filter", "kernel_filter", "filter_strength"], default = "main")
+    parser.add_argument("-image", type=str, default="building")
     
     args = parser.parse_args()
     mode = args.mode
+    task = args.task
+    imageName = "img/{}.png".format(args.image)
    
     if mode == "show":
-        show_main()
-    
-    elif mode == "sobel":
-        sobel_main()
+        show_main(imageName)
+
+    elif mode == "compare_gradient":
+        compare_gradient_main(imageName)
+
+    else:
+        # Determine method
+        if mode == "sobel":
+            method = sobel_edge
+        
+        elif mode == "naive_gradient":
+            method = naive_gradient
+        
+        elif mode == "scharr":
+            method = scharr_edge
+
+        elif mode == "stacking":
+            method = stacking
+
+        # Determine task
+        if task == "main":
+            method_main(imageName, method)
+
+        elif task == "threshold":
+            threshold_main(imageName, method)
+
+        elif task == "filter":
+            filtering_main(imageName, method)
+
+        elif task == "kernel_filter":
+            filter_kernel_main(imageName, method)
+
+        elif task == "filter_strength":
+            filter_strength_main(imageName, method)
