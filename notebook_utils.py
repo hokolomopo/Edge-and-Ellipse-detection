ff12e071908746ace8bf74fd9d93960333fcb383
hokolomopo <adrien.minne@outlook.com>
2019-11-01 15:12:44 +0100

    Add interactions in notebook

---
+++ notebook_utils.py
@@ -0,0 +1,50 @@
+import cv2
+import numpy as np
+import tools
+import matplotlib.cm as cm   
+from ipywidgets import interact, fixed
+
+from image import *
+from edge import *
+from line import *
+
+
+def update_edges(method="sobel", image="building", low_filtering = True, low_filter_type = "gaussian", low_filtering_kernel_size = 5, 
+          high_filtering = False, high_filter_type = "gaussian", high_filtering_kernel_size = 3, strength = 2.0, 
+          threshold = True, block_size=11, constant=3,
+          edge_threshold=30):
+    
+    img = load_gray_img("img/{}.png".format(image))
+
+    img_filtered = filtering(img, low_filtering, low_filter_type, low_filtering_kernel_size,
+                        high_filtering, high_filter_type, high_filtering_kernel_size, strength)
+
+    img_threshold = img_filtered
+    if threshold:
+        img_threshold = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                cv2.THRESH_BINARY, block_size, constant)
+
+    if method == "sobel":
+        grad = sobel_edge(img_threshold, thresholding = True, threshold = edge_threshold)
+    
+    elif method == "naive_gradient":
+        grad = naive_gradient(img_threshold, thresholding = True, threshold = edge_threshold)
+    
+    elif method == "scharr":
+        grad = scharr_edge(img_threshold, thresholding = True, threshold = edge_threshold)
+
+    elif method == "stacking":
+        grad = stacking(img_threshold, thresholding = True, threshold = edge_threshold)
+
+    elif method == "beucher":
+        grad = beucher_edge(img_threshold, thresholding = True, threshold = edge_threshold)
+
+    elif method == "canny":
+        grad = beucher_edge(img_threshold, thresholding = True, threshold = edge_threshold)
+
+
+    tools.multiPlot(1, 4, 
+            (img, img_filtered, img_threshold, grad),
+            ('Original Image', 'Filtered Image', 'Thresholded Image', 'Edges'),
+            cmap_tuple=(cm.gray, cm.gray, cm.gray, cm.gray, cm.gray, cm.gray))   
+
