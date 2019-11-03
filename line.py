be6b477324e0f995975203f2ea60e9af03911da7
adrien <adrien.bolland@student.uliege.be>
2019-11-03 11:07:24 +0100

    removed the contour following method (to edge.py)

--- line.py
+++ line.py
@@ -68,27 +68,33 @@ def hough_probabilist(edge_img, img_print, rho=1, theta=np.pi / 180,
     return lines[:, 0]
 
 
-def contours(img, img_print, thresh=240, maxval=255):
+def get_edges_on_lines(edges, img_w_lines, kernel_size=3):
     """
-    Returns the contours and draw them on the second argument
+    Get the edges that belong to a line in an image
 
     Parameters
     ----------
 
-    - img :       The image on which to detect the contour
-    - img_print : Image on which to draw the contour
-    - thresh :    Threshold of intensity in the image
-    - maxval :    Maximal value in the img
+    - edges :           The edges of the image
+    - img_w_lines :     Image with all the lines drawed
+    - kernel_size :     Size of the kernel to determine if a point of an edge is
+                        on a line
 
     Return
     ------
-    The contour of the image
+    The image containing only the edges points on a line
     """
-    _, thresh = cv2.threshold(img, thresh, maxval, 0)
-    contours, _ = cv2.findContours(thresh,
-                                   cv2.RETR_TREE,
-                                   cv2.CHAIN_APPROX_NONE)
+    final = np.zeros(edges.shape)
+    kernel = np.ones((kernel_size, kernel_size))
+    r = cv2.filter2D(img_w_lines, -1, kernel, borderType=cv2.BORDER_CONSTANT)
 
-    cv2.drawContours(img_print, contours, -1, (255, 0, 0), 1)
+    for i in range(edges.shape[0]):
+        for j in range(edges.shape[1]):
+            if(edges[i][j] == 0):
+                continue
+            if(r[i][j] > 0):
+                final[i][j] = 255
+            else:
+                final[i][j] = 0
 
-    return contours
+    return final
